691c3a06a934a27bcbf0bd4832e5e7c1fc9a4333
Rishi.R.Vishwakarma <149783805+RishiWebDeveloper1@users.noreply.github.com>
2024-05-19 23:21:34 +0530

    Update C-test.c

--- C-test.c
+++ C-test.c
@@ -1,1 +1,12 @@
-// this is test logic program
+#include<stdio.h>
+#include<conio.h>
+
+void main() {
+  clrscr();
+
+  int var1 = 10;
+
+  while( i < 100 ) {
+      i = i+10;
+      printf("%d", i);
+}
